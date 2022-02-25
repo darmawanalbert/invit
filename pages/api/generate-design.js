62286b31d43e0849780ca697baa97b9e7ca85ff4
Albert Darmawan <darmawanalbert@gmail.com>
2022-02-26 01:44:10 +1100

    dev: POST request for main API

--- pages/api/generate-design.js
+++ pages/api/generate-design.js
@@ -2,15 +2,9 @@ import initialPopulation from '../../utilities/initialPopulation';
 import generateBackground from '../../utilities/generateBackground';
 
 export default (req, res) => {
-    // if (req.method === 'POST') {
-    if (req.method === 'GET') {
-        // Retrieve all request body values
-        // const partnerOneName = req.body.partnerOneName;
-        // const partnerTwoName = req.body.partnerTwoName;
-        // const dateString = req.body.dateString;
-        // const timeString = req.body.timeString;
-
-        const populationList = initialPopulation(1);
+    if (req.method === 'POST') {
+        const sessionId = req.body.sessionId;
+        const populationList = initialPopulation(20);
         const bgBase64List = [];
         for (let i = 0; i < populationList.length; i++) {
             const individual = populationList[i];
@@ -18,7 +12,7 @@ export default (req, res) => {
             bgBase64List.push(bgBase64);
         }
 
-        res.status(200).json({ populationList: populationList, bgBase64List: bgBase64List });
+        res.status(200).json({ bgBase64List: bgBase64List });
     } else {
         // 405: Method Not Allowed
         res.status(405).end();
