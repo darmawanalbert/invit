a2d92d0014e35400f255b7e9c14e5467db02f668
cakraocha <cakraocha@gmail.com>
2022-02-26 17:59:00 +1100

    feat: twilio server with params and msg format

--- twilio/app/main.py
+++ twilio/app/main.py
@@ -1,8 +1,10 @@
 from fastapi import FastAPI, APIRouter, Request
 
+from pydantic import BaseModel, HttpUrl
+
 import requests
 
-import json
+import os
 
 app = FastAPI(
     title="ALign Twilio",
@@ -12,23 +14,30 @@ app = FastAPI(
 
 api_router = APIRouter(prefix="/align/twilio")
 
-with open("env.json", "r") as envfile:
-    twilio_creds = json.load(envfile)
+class Invitation(BaseModel):
+    num: str
+    inviter: str
+    invitee: str
+    invitation_url: HttpUrl
 
-account_sid = twilio_creds["main"]["accountSid"]
-auth_token = twilio_creds["main"]["authToken"]
+account_sid = os.environ.get("TWILIO_SID")
+auth_token = os.environ.get("TWILIO_AUTH_TOKEN")
 
 @api_router.get("/", status_code=200)
 async def root() -> dict:
     return {"msg": "Welcome to ALign Twilio API"}
 
-@api_router.post("/invite", status_code=200)
+@api_router.post("/invite", status_code=200, response_model=Invitation)
 async def invite(request: Request) -> dict:
     req = await request.json()
     phone = req["num"]
+    inviter = req["inviter"]
+    invitee = req["invitee"]
+    invitation_url = req["invitation_url"]
+    body = f"Hi {invitee},\nYou are invited to our wedding, {inviter}. Please see {invitation_url} to see the invitation. :)"
     url = "https://api.twilio.com/2010-04-01/Accounts/AC66f01d289767252436f377e1af928b34/Messages.json"
     params = {
-        "Body": "Bella main yuk ini yang kedua",
+        "Body": body,
         "From": "+19034626259",
         "To": phone
     }
