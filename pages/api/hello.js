b562d091684d1fe3a3e799bad34e7fe75d7ade85
Albert Darmawan <darmawanalbert@gmail.com>
2022-02-27 02:36:45 +1100

    colorContrast calculation

--- pages/api/hello.js
+++ pages/api/hello.js
@@ -1,12 +1,12 @@
-import crossover from '../../utilities/crossover';
+import colorContrast from "../../utilities/colorContrast";
 
 export default async (req, res) => {
     if (req.method === 'GET') {
-        const arr = [46, 106, 134, 175, 211, 129, 38, 1, 32, 94, 92, 41];
-        const arr2 = [1, 2, 3, 4, 5, 6, 7, 8, 9, 10, 11, 12];
-        const [newArr, newArr2] = crossover(arr, arr2);
-
-        res.status(200).json({ newArr: newArr, newArr2: newArr2 });
+        // 8.592 for blue
+        const blueRatio = colorContrast([255, 255, 255], [0, 0, 255]);
+        // 1.074 for yellow
+        const yellowRatio = colorContrast([255, 255, 255], [255, 255, 0]);
+        res.status(200).json({ blueRatio: blueRatio, yellowRatio: yellowRatio });
     } else {
         // 405: Method Not Allowed
         res.status(405).end();
