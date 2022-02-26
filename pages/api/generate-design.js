74b5a326cfa1a434d6ccdedc5f4442bef81af83d
Albert Darmawan <darmawanalbert@gmail.com>
2022-02-26 11:58:15 +1100

    dev: Main data flow

--- pages/api/generate-design.js
+++ pages/api/generate-design.js
@@ -6,22 +6,8 @@ import rgbToHex from '../../utilities/rgbToHex';
 
 export default async (req, res) => {
     if (req.method === 'POST') {
+        // Receive value from request body
         const sessionId = req.body.sessionId;
-        const populationList = initialPopulation(10);
-        const canvasObject = initializeCanvas();
-        const invitationList = [];
-        for (let i = 0; i < populationList.length; i++) {
-            const individual = populationList[i];
-            const bgBase64 = generateBackground(canvasObject, individual.slice(0,3), individual.slice(3,6), individual[6], individual[7]);
-            const textColor = rgbToHex(individual[8], individual[9], individual[10]);
-            const textSize = individual[11];
-            const invitationObject = {
-                bgBase64: bgBase64,
-                textColor: textColor,
-                textSize: textSize,
-            }
-            invitationList.push(invitationObject);
-        }
 
         // Initialize Firebase Admin instance
         const serviceAccount = JSON.parse(process.env.FIREBASE_SERVICE_ACCOUNT_KEY);
@@ -35,12 +21,56 @@ export default async (req, res) => {
 
         // Initialize connection to Firebase Realtime Database
         const db = admin.firestore();
-        const sampleRef = db.collection('invitations').doc('first-sample');
-        const doc = await sampleRef.get();
-        if (!doc.exists) {
-            console.log("The document doesn't exist1");
+        const sessionRef = db.collection('invitations').doc(sessionId);
+        const sessionDoc = await sessionRef.get();
+        const invitationList = []
+        if (!sessionDoc.exists) {
+            console.log("The document doesn't exist");
+            // Create new population
+            const populationList = initialPopulation(10);
+
+            // Save the new population to Firestore
+            const res = await sessionRef.set({ ...populationList });
+
+            // Infer all properties in each individual, then put it into invitationList
+            const canvasObject = initializeCanvas();
+            for (let i = 0; i < populationList.length; i++) {
+                const individual = populationList[i];
+                const bgBase64 = generateBackground(canvasObject, individual.slice(0,3), individual.slice(3,6), individual[6], individual[7]);
+                const textColor = rgbToHex(individual[8], individual[9], individual[10]);
+                const textSize = individual[11];
+                const invitationObject = {
+                    bgBase64: bgBase64,
+                    textColor: textColor,
+                    textSize: textSize,
+                }
+                invitationList.push(invitationObject);
+            }
         } else {
-            console.log(doc.data());
+            console.log("The document already exists!");
+            const currentPopulation = Object.values(sessionDoc.data())
+            console.log(currentPopulation);
+            // Perform GA operations to generate the next generations
+            // TODO: Change this, for now let's use initial population
+            const newPopulationList = initialPopulation(10);
+
+            // Save the new population to Firestore
+            const res = await sessionRef.set({ ...newPopulationList });
+
+            // Infer all properties in each individual, then put it into invitationList
+            const canvasObject = initializeCanvas();
+            for (let i = 0; i < newPopulationList.length; i++) {
+                const individual = newPopulationList[i];
+                const bgBase64 = generateBackground(canvasObject, individual.slice(0,3), individual.slice(3,6), individual[6], individual[7]);
+                const textColor = rgbToHex(individual[8], individual[9], individual[10]);
+                const textSize = individual[11];
+                const invitationObject = {
+                    bgBase64: bgBase64,
+                    textColor: textColor,
+                    textSize: textSize,
+                }
+                invitationList.push(invitationObject);
+            }
         }
 
         res.status(200).json({ invitationList: invitationList });
